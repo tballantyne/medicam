2cfe19aa1306392de8e725de89eb07c89b61a13f
Greg Hughes <greg@fam.computer>
2020-03-18 16:28:36 -0700

    fix DATABASES

--- medicam/settings.py
+++ medicam/settings.py
@@ -78,7 +78,9 @@ WSGI_APPLICATION = 'medicam.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600),
+}
 
 
 # Password validation
