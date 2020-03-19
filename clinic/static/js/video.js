2ae783836b65f5a1a819283900d1ecefc09e7990
Greg Hughes <greg@fam.computer>
2020-03-18 22:08:47 -0700

    show camera preview

--- clinic/static/js/video.js
+++ clinic/static/js/video.js
@@ -1,4 +1,9 @@
 function initVideo(token, room) {
+	Twilio.Video.createLocalVideoTrack().then(track => {
+		const localMediaContainer = document.getElementById('local-media');
+		localMediaContainer.appendChild(track.attach());
+	});
+
 	Twilio.Video.connect(token, {
 		name: room,
 		audio: true,
@@ -7,6 +12,7 @@ function initVideo(token, room) {
 		console.log(`Successfully joined a Room: ${room}`);
 		room.on('participantConnected', participant => {
 			console.log(`A remote Participant connected: ${participant}`);
+			document.getElementById('connection-status').innerText = "Connected";
 			participant.tracks.forEach(publication => {
 				if (publication.isSubscribed) {
 					const track = publication.track;
@@ -16,6 +22,10 @@ function initVideo(token, room) {
 			participant.on('trackSubscribed', track => {
 				document.getElementById('remote-media').appendChild(track.attach());
 			});
+		});
+		room.on('participantDisconnected', participant => {
+  			console.log(`Participant disconnected: ${participant.identity}`);
+  			document.getElementById('connection-status').innerText = "Disconnected";
 		});
 	}, error => {
 		console.error(`Unable to connect to Room: ${error.message}`);
