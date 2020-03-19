d42374e4fd7ff94f81c3dedfbf5d7732fc12a5c9
Greg Hughes <greg@fam.computer>
2020-03-18 17:07:21 -0700

    add STATIC_ROOT

--- medicam/settings.py
+++ medicam/settings.py
@@ -125,6 +125,7 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
